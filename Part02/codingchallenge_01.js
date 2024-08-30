388ca8298d3cd7604d38c45bdec713e8d2661770
H51M14\Student <Student@gmail.com>
2024-08-30 08:14:59 +0700

    Part02 - Challenge01

---
+++ Part02/codingchallenge_01.js
@@ -0,0 +1,28 @@
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+// gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so
+// one average score per team).
+// A team only wins if it has at least double the average score of the other team.
+// Otherwise, no team wins! 
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const avgDolphins1 = calcAverage(44, 23, 71);
+const avgKoalas1 = calcAverage(65, 54, 49);
+
+const avgDolphins2 = calcAverage(85, 54, 41);
+const avgKoalas2 = calcAverage(23, 34, 27);
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log('No team wins!');
+    }
+};
+console.log('--- Data Set 1 ---');
+checkWinner(avgDolphins1, avgKoalas1);
+console.log('--- Data Set 2 ---');
+checkWinner(avgDolphins2, avgKoalas2);
